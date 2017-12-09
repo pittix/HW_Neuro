c7161379f1c2ed441c83c7e33150c75f38dde3dd
Andrea Pittaro <pittaroa@dei.unipd.it>
2017-12-09 14:28:03 +0100

    aggiunto parzialmente il codice task3

--- HW.m
+++ HW.m
@@ -4,21 +4,41 @@ FOLDER_NAME = 'A2MB';
 data=load_data(FOLDER_NAME); %1 patient per struct and each struct has the data
 TR=2.6; %s
 A=data(1).ROI;
+numPazienti = size(data,2);
+%% Load alternativo
+load HW9_data.mat
 
-filtered = dataFilter(data(1).ROI); %check errors
+
+
+%% task 2 - deriva temporale
+
+% filtraggio segnali
+for paziente =1:1:numPazienti %per ogni paziente, filtra le ROI
+    ROIfiltrate(paziente,:,:) = dataFilter(data(paziente).ROI,'butter'); %check errors
+    [media(paziente),varianza(paziente)] = analisiDeriva(ROIfiltrate(paziente));
+end
+
 
 
 %% task 3
 %derivata motion e padding
 motion_diff = [diff(data(1).motion,1,1) ; zeros(1,size(data(1).motion,2))];
-idxCSF = find(data(1).explVarCSF>70);
-idxWM = find(data(1).explVarWM>50);
+idxCSF = find(data(1).explVarCSF>70,1,'first');
+idxWM = find(data(1).explVarWM>50,1,'first');
 
 %matrice per regressione 225*24
 %        1                                  6           6   
-X= [ones(size(diff(data(1).motion,1),1)),data(1).motion,motion_diff, data(1).CSF(:,[1,idxCSF(1)]),data(1).WM(:,[1,idxWM(1)])];
-
-Y= 0 ;
+% X= [ones(size(diff(data(1).motion,1),1)),data(1).motion,motion_diff, data(1).CSF(:,[1,idxCSF(1)]),data(1).WM(:,[1,idxWM(1)])];
+betas = cell(numPazienti,size(data(1).ROI,1));
+for paziente = 1:1:numPazienti
+    motion_diff = [diff(data(paziente).motion,1,1) ; zeros(1,size(data(paziente).motion,2))];
+   X=[data(paziente).motion, data(paziente).motion.^2, motion_diff, motion_diff.^2]; 
+   for acq=1:1:size(data(paziente).ROI,1)
+       Y=data(paziente).ROI(acq).tac_filtered;
+       betas{paziente,acq} =  (X'*X)\X'*Y;
+   end
+   
+end
 
 beta;
 
@@ -42,60 +62,3 @@ end
 
 
 
-%%
-Fs = 0.38461538462;  % Sampling Frequency
-Fstop = 0.005;       % Stopband Frequency
-Fpass = 0.006;       % Passband Frequency
-Astop = 80;          % Stopband Attenuation (dB)
-Apass = 1;           % Passband Ripple (dB)
-match = 'passband';  % Band to match exactly
-
-% Construct an FDESIGN object and call its BUTTER method.
-h  = fdesign.highpass(Fstop, Fpass, Astop, Apass, Fs);
-Hd = design(h, 'butter', 'MatchExactly', match);
-
-filt = filter(Hd,data(1).ROI(1).tac);
-
-
-%% OK - stesso prof
-Fs = 0.38461538462;  % Sampling Frequency
-
-Fstop = 0.005;   % Stopband Frequency
-Fpass = 0.006;   % Passband Frequency
-Astop = 7;      % Stopband Attenuation (dB)
-Apass = 1;       % Passband Ripple (dB)
-match = 'both';  % Band to match exactly
-
-% Construct an FDESIGN object and call its ELLIP method.
-h  = fdesign.highpass(Fstop, Fpass, Astop, Apass, Fs);
-Hd = design(h, 'ellip', 'MatchExactly', match);
-
-filt = filter(Hd,data(3).ROI(2).tac);
-
-figure(1)
-plot(filt)
-hold on
-plot(data(3).ROI(2).tac_filtered)
-hold off
-%% 
-
-% %% plot all series of all data translation and rotation
-% for i=1:1:numFolders
-%     subplot(2,3,1)
-%     plot(motionRegressors{i}(:,1))
-%     subplot(2,3,2)
-%     plot(motionRegressors{i}(:,2))
-%     subplot(2,3,3)
-%     plot(motionRegressors{i}(:,3))
-%     subplot(2,3,4)
-%     plot(motionRegressors{i}(:,4))
-%     subplot(2,3,5)
-%     plot(motionRegressors{i}(:,5))
-%     subplot(2,3,6)
-%     plot(motionRegressors{i}(:,6))
-%     pause(1)
-% 
-% %     for j=1:1:size(motionRegressors{1},2)
-% %         
-% %     end
-% end
--- dataFilter.m
+++ dataFilter.m
@@ -1,24 +1,53 @@
-function [filtered] = dataFilter(sig)
+function [filtered] = dataFilter(sig,varargin)
 %filter all data for a patient
-
-% Butterworth Highpass filter designed using FDESIGN.HIGHPASS.
-
-% All frequency values are in Hz.
-Fs = 0.38461538462;  % Sampling Frequency
-
-Fstop = 0.005;   % Stopband Frequency
-Fpass = 0.006;   % Passband Frequency
-Astop = 80;      % Stopband Attenuation (dB)
-Apass = 1;       % Passband Ripple (dB)
-match = 'both';  % Band to match exactly
-
-% Construct an FDESIGN object and call its ELLIP method.
-h  = fdesign.highpass(Fstop, Fpass, Astop, Apass, Fs);
-Hd = design(h, 'ellip', 'MatchExactly', match);
-
-% filtered = zeros(size(sig,1),size(sig,1),1);
-for i=1:1:size(sig,1)
-    tac = sig(i).tac;
-    filtered(i) = filter(Hd,tac);
+TR=2.6; %sec
+tipo='';
+filtered = zeros(size(sig,2),size(sig(1).tac,1));
+if(nargin == 1)
+   tipo = 'ellip'; 
+else
+    if(strcmp(varargin{1},'ellip') || strcmp(varargin{1},'butter'))
+        tipo = varargin{1};
+    else 
+        err('Il tipo di filtro deve essere o "ellip" o "butter" ');
+    end
 end
+% Butterworth Highpass filter designed using FDESIGN.HIGHPASS.
+% All frequency values are in Hz.
+if( strcmp(tipo,'ellip'))
+    
+
+    Fs = 1/TR;  % Sampling Frequency
+
+    Fstop = 0.005;   % Stopband Frequency
+    Fpass = 0.006;   % Passband Frequency
+    Astop = 80;      % Stopband Attenuation (dB)
+    Apass = 1;       % Passband Ripple (dB)
+    match = 'both';  % Band to match exactly
+
+    % Construct an FDESIGN object and call its ELLIP method.
+    h  = fdesign.highpass(Fstop, Fpass, Astop, Apass, Fs);
+    Hd = design(h, 'ellip', 'MatchExactly', match);
+    for i=1:1:size(sig,1)
+        filtered(i,:) = filter(Hd,sig(i).tac)';
+    end
+    
+elseif  strcmp(tipo,'butter') %%use butterworth
+    Fs=1/TR;
+    Wp=0.006/(Fs/2);
+    Ws=0.003/(Fs/2);
+    Rp=0.5; %dB                   
+    Rs=25; %dB
+    [ord, Wn]=buttord(Wp,Ws,Rp,Rs);
+    [B,A]=butter(ord,Wn,'high');
+    for i=1:1:size(sig,1)
+        filtered(i,:)=filtfilt(B,A,sig(i).tac)';
+    end
+else
+    err('Il tipo di filtro deve essere o "ellip" o "butter" ');
+end
+ % filtered = zeros(size(sig,1),size(sig,1),1);
+
+
+
 end

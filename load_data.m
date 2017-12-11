e9e8bd8445370627f8048469515c28a3412cab0d
Andrea <pittix@live.it>
2017-12-11 14:02:18 +0100

    corrected load_data to reduce time loading

--- load_data.m
+++ load_data.m
@@ -9,27 +9,14 @@ folder_list = dir(folder);
 %(sotto Linux)
 numFolders = length(folder_list)-2; %numero di soggetti. 
 
-% caricamento variabili
-% CSF_princ = cell(numFolders,1);
-% motionRegressors = cell(numFolders,1);
-% WM_princ = cell(numFolders,1);
-% explVarCSF = cell(numFolders,1);
-% explVarWM = cell(numFolders,1);
-% malf_ROI = cell(numFolders,1);
+%inizializzazione struct
+data(numFolders) = struct('ROI',0,'CSF',0,'WM',0,'explVarCSF',0,'explVarWM',0,'motion',0);
 
-% data = zeros(numFolders,1);
-for i=1:1:numFolders
+ for i=1:1:numFolders
     folder_list_path = strcat(folder_list(i+2).folder, '/', folder_list(i+2).name, '/');   
     load(strcat(folder_list_path,TIMESERIES_NAME));
     load(strcat(folder_list_path,MOT_EST_NAME));
-%inutile se usiamo la struct
-%     malf_ROI{i}         = malfROI;
-%     CSF_princ{i}        = CSFprinc;
-%     WM_princ{i}         = WMprinc;
-%     explVarCSF{i}       = explained_variance_CSF;
-%     explVarWM{i}        = explained_variance_WM;
-%     motionRegressors{i} = Motion_regressors;
-    
+   
     dataTmp = struct('ROI',malfROI, 'CSF',CSFprinc, 'WM', WMprinc,...
         'explVarCSF',explained_variance_CSF, 'explVarWM',explained_variance_WM,...
         'motion',Motion_regressors);
@@ -37,9 +24,6 @@ for i=1:1:numFolders
     
     clear malfROI CSFprinc WMprinc explained_variance_CSF explained_variance_WM Motion_regressors
 end
-
-% data = struct('ROI','malf_ROI', 'CSF','CSF_princ', 'WM', 'WM_princ');
-% data.add
 
 end
 
